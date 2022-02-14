53ef31fef5a8cb11c45f8d1b3dc6f07ab353ea99
Harsh-Sisodia857 <Harsh2012007@akgec.ac.in>
2022-02-15 00:36:00 +0530

    implementing partials

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,10 +1,10 @@
-module.exports.profile = function(req,res){
+module.exports.user = function(req,res){
     res.render('user',{
         title : "Profile"
     })
 
 }
 
-module.exports.user = function(req,res){
-    res.end("<h1>User's Page</h1>");
+module.exports.profile = function(req,res){
+    res.end("User's Profile")
 }
