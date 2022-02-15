52ee3c5244fdc7fc830e8937c79faa56a43609a1
Harsh-Sisodia857 <Harsh2012007@akgec.ac.in>
2022-02-15 10:26:59 +0530

    Setup Asset folder which include css,js, images file

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const expressLayout = require('express-ejs-layouts')
 
 app.use(expressLayout);
 
+// static files
+app.use(express.static('./assets'))
+
 // use express router
 app.use('/',require('./routes/index'))
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
+
 
 // setting view engine 
 app.set('view engine','ejs')
